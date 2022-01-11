b71a0cc578076bcf816af129ab3c697357b575fa
Alberto <aldo10012@gmail.com>
2022-01-11 10:19:05 -0800

    added new project foler

---
+++ Widget-prac/Widget-prac/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Widget-prac
+//
+//  Created by Alberto Dominguez on 1/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
