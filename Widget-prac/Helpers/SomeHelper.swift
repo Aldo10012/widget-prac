d1b0df438abfd4822221ac290d635a2270c157b4
Alberto <aldo10012@gmail.com>
2022-01-11 09:54:17 -0800

    app setup

---
+++ Widget-prac/Helpers/SomeHelper.swift
@@ -0,0 +1,8 @@
+//
+//  SomeHelper.swift
+//  Widget-prac
+//
+//  Created by Alberto Dominguez on 1/11/22.
+//
+
+import Foundation
---
+++ Widget-prac/ViewModel/SomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SomeViewModel.swift
+//  Widget-prac
+//
+//  Created by Alberto Dominguez on 1/11/22.
+//
+
+import Foundation
